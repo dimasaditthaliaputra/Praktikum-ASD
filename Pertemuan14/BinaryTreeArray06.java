ca12baa7fe0e5ea4066f0930bd62351027e775f3
dimasaditt <aditcraftyt@gmail.com>
2025-06-04 21:35:03 +0700

    tugas no 4 modifikasi binaryArray

--- Pertemuan14/BinaryTreeArray06.java
+++ Pertemuan14/BinaryTreeArray06.java
@@ -4,11 +4,21 @@ public class BinaryTreeArray06 {
 
     public BinaryTreeArray06() {
         this.dataMahasiswa = new Mahasiswa06[10];
+        this.idxLast = -1;
     }
 
     void populateData(Mahasiswa06 dataMhs[], int idxLast) {
         this.dataMahasiswa = dataMhs;
         this.idxLast = idxLast;
+    }
+
+    public void add(Mahasiswa06 data) {
+        if (idxLast < dataMahasiswa.length - 1) {
+            idxLast++;
+            dataMahasiswa[idxLast] = data;
+        } else {
+            System.out.println("Tree penuh");
+        }
     }
 
     void traverseInOrder(int idxStart) {
@@ -18,6 +28,14 @@ public class BinaryTreeArray06 {
                 dataMahasiswa[idxStart].tampilInformasi();
                 traverseInOrder(2 * idxStart + 2);
             }
+        }
+    }
+
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast && dataMahasiswa[idxStart] != null) {
+            dataMahasiswa[idxStart].tampilInformasi(); 
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
         }
     }
 }
